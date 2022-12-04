0de715583f23024d352c7b129d73db0d9d36bb1f
Aigul <aigul.sibgatulli@mail.ru>
2022-12-04 17:05:06 +0300

    2022 day 4

---
+++ 2022/Day 4/4.py
@@ -0,0 +1,18 @@
+import re
+
+with open('4.txt', 'r') as f:
+    counter = 0
+    counter2 = 0
+    for line in f.readlines():
+        f_start, f_end, s_start, s_end = [int(num) for num in re.split('[,-]', line)]
+        first_set = set([i for i in range(f_start, f_end + 1)])
+        second_set = set([i for i in range(s_start, s_end + 1)])
+        overlap = first_set & second_set
+        # if first_set.issubset(second_set) or second_set.issubset(first_set):
+        if len(overlap) in (len(first_set), len(second_set)):
+            counter += 1
+        if len(overlap) != 0:
+            counter2 += 1
+
+print(counter)
+print(counter2)

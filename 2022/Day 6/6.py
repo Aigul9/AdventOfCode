aee43aeed215502e74ac84184c5bdbd7762d8d0a
Aigul <aigul.sibgatulli@mail.ru>
2022-12-07 19:43:15 +0300

    2022 day 6

---
+++ 2022/Day 6/6.py
@@ -0,0 +1,28 @@
+import timeit
+
+
+def fun(n):
+    for i in range(n, len(line)):
+        sub = line[i-n:i]
+        if len(set(sub)) == len(sub):
+            return i
+
+
+start = timeit.default_timer()
+
+tests = [
+    'mjqjpqmgbljsphdztnvjfqwrcgsmlb',  # 7
+    'bvwbjplbgvbhsrlpgdmjqwftvncz',  # 5
+    'nppdvjthqldpwncqszvftbrmjlhg',  # 6
+    'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg',  # 10,
+    'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'  # 11
+]
+
+with open('6.txt', 'r') as f:
+    line = f.read()
+
+print(fun(4))
+print(fun(14))
+
+end = timeit.default_timer()
+print(end - start)

e93c7124ffce790cd1e667c70b2c47ec6ff73289
Сибгатуллина Айгуль Ильдаровна <AIlSibgatullina@sberbank.ru>
2023-12-01 14:55:48 +0300

    remove replaced flag

--- year2023/Day_01/main.py
+++ year2023/Day_01/main.py
@@ -26,7 +26,6 @@ def part1():
 
 def get_substring(input_line, is_end=False):
     new_line_part = ''
-    replaced = False
 
     for char in input_line:
         new_line_part = new_line_part + char if not is_end else char + new_line_part
@@ -34,10 +33,8 @@ def get_substring(input_line, is_end=False):
         for k, v in digit_dict.items():
             if k in new_line_part:
                 new_line_part = new_line_part.replace(k, v)
-                replaced = True
-                break
-        if replaced:
-            break
+                return new_line_part
+
     return new_line_part
 
 
@@ -48,7 +45,7 @@ def part2():
         total = 0
 
         for line in lines:
-            new_line_start = get_substring(line, False)
+            new_line_start = get_substring(line)
             new_line_end = get_substring(line[::-1], True)
             new_line = new_line_start + new_line_end
             digits = ''.join([char for char in new_line if char.isdigit()])

0f0643709759443e3a28b0a9275218d6a1322406
Aigul <46625461+Aigul9@users.noreply.github.com>
2023-12-05 09:16:27 +0300

    2023 day 05 part1

--- year2023/Day_05/main.py
+++ year2023/Day_05/main.py
@@ -1,14 +1,10 @@
 from utils.measure import timeit, memory
 
 
-def get_mapped_values(input_list, seeds_input):
+def get_mapped_values(input_list, prev_value):
     dst, src, rng = [int(i) for i in input_list.split()]
-    zipped = list(zip(range(dst, dst + rng), range(src, src + rng)))
-    for key, value in seeds_input.items():
-        res = [d for d, s in zipped if s == value]
-        if res:
-            seeds_input[key] = res[0]
-    return seeds_input
+    if src <= prev_value < src + rng:
+        return dst + (prev_value - src)
 
 
 def part1(path):
@@ -19,12 +15,16 @@ def part1(path):
         seeds_dict = {int(key): int(key) for key in seeds}
         for line in lines:
             map_values = line.split('\n')[1:]
-            for row in map_values:
-                seeds_dict = get_mapped_values(row, seeds_dict)
+            for key, value in seeds_dict.items():
+                for row in map_values:
+                    new_dst = get_mapped_values(row, value)
+                    if new_dst:
+                        seeds_dict[key] = get_mapped_values(row, value)
+                        break
 
-        print(seeds_dict)
+        return min(v for v in seeds_dict.values())
 
 
 if __name__ == '__main__':
-    # assert part1('test.txt') == 13
+    assert part1('test.txt') == 35
     print(part1('input.txt'))

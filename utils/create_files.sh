0bb6b8c2eb62f2263bf9fd5dfb1d47dce04631f5
Aigul <46625461+Aigul9@users.noreply.github.com>
2023-12-08 22:51:42 +0300

    add bash script for creating new files

---
+++ utils/create_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+read -p "year: " year
+# user="USER INPUT"
+read -p "day in 2-digit format: " day
+
+year_folder="year$year"
+day_folder="Day_$day"
+
+cd $year_folder
+
+mkdir $day_folder
+
+cd $day_folder
+
+touch input.txt
+touch test.txt
+touch main.py
+touch task.txt
+
+echo "done"
+
+read
